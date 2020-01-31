22f1b7fab611831d95dabe8c712da2316eb7a949
klangner <klangner@gmail.com>
2020-01-31 23:27:48 +0100

    init

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,36 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+
+  /**
+   * Create an Action to render an HTML page.
+   *
+   * The configuration in the `routes` file means that this method
+   * will be called when the application receives a `GET` request with
+   * a path of `/`.
+   */
+  def index() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index())
+  }
+
+  def stations() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.stations())
+  }
+
+  def rainfall() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.rainfall())
+  }
+
+  def about() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.about())
+  }
+}
