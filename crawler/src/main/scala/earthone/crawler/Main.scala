10263907d911c5ab638a2ffe3d08afcd76cf796e
klangner <klangner@gmail.com>
2020-02-08 11:58:10 +0100

    initial crawler based on akka http

--- crawler/src/main/scala/earthone/crawler/Main.scala
+++ crawler/src/main/scala/earthone/crawler/Main.scala
@@ -1,5 +1,28 @@
 package earthone.crawler
 
-object Main extends App {
-  println("Crawl public data")
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.stream.ActorMaterializer
+
+import scala.concurrent.Future
+import scala.util.{ Failure, Success }
+
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    implicit val system = ActorSystem()
+    implicit val materializer = ActorMaterializer()
+    implicit val executionContext = system.dispatcher
+
+    val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = "http://akka.io"))
+
+    responseFuture
+      .onComplete {
+        case Success(res) => 
+          println(res)
+          res.entity.discardBytes()
+        case Failure(_)   => sys.error("something wrong")
+      }
+  }
 }
