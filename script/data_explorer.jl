56ada42e91949b93e2e877e3f6813df001947190
klangner <klangner@gmail.com>
2020-04-15 20:15:44 +0200

    julia interact ver 1.0

--- script/data_explorer.jl
+++ script/data_explorer.jl
@@ -1,33 +1,12 @@
-using Mux, WebIO
 using Interact, Plots
+using CSV
 
 
 const CHANNEL_NAMES = ["water", "rain", "winddir", "windlevel", "temp", "pressure", "humidity", "sun"]
+stations = CSV.read("data/gdanskiewody/stations.csv"; truestrings=["True"], falsestrings=["False"])
 
-function app(req) # req is a Mux request dictionary
-    px=widget(0:0.01:.3, label="px")
-    py=widget(0:0.01:.3, label="py")
-    plane = widget(Dict("x=0" => 1, "y=0" => 2), label="Section plane")
-    interactive_plot = map(plotsos, px, py, plane)
-    return vbox(
-        hbox(px, py, plane),
-        interactive_plot)
+
+@manipulate for channel in CHANNEL_NAMES, station in stations[stations.water, :no]
+    df = CSV.read("data/gdanskiewody/$station-$channel.csv"; dateformat="yyyy-mm-dd HH:MM:SS")
+    scatter(df[!, :time], df[!, :water], title="Station $station", marker=:auto)
 end
-
-function dashboard(req)
-    options = Observable(CHANNEL_NAMES)
-    return hbox(
-        vbox(
-            dropdown(CHANNEL_NAMES),
-            dropdown(options))
-            
-    )
-end
-
-
-# Start server
-webio_serve(page("/", dashboard), 8000)
-# Keep program running
-print("Server started. Press Enter to stop.")
-readline(stdin)
-print("bye.\n")
