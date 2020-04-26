2ae3183a92fadbd3816d9df76d69f81a9e067878
klangner <klangner@gmail.com>
2020-04-26 12:17:34 +0200

    find missing runs from sbt

--- src/main/scala/earth1/FindMissingReadings.scala
+++ src/main/scala/earth1/FindMissingReadings.scala
@@ -1,38 +1,45 @@
 package earth1
 
-import org.apache.spark.sql.{SparkSession}
+import org.apache.spark.sql.{SparkSession, DataFrame}
 import org.apache.spark.sql.types._
 
 
-object FindMissingReadings extends App {
+object FindMissingReadings {
 
-  val spark = SparkSession.builder()
-    .master("local[1]")
-    .appName("FindMissingReadings")
-    .getOrCreate();
-
-  spark.sparkContext.setLogLevel("ERROR")
-
-  val stations = spark
+  def loadStations(spark: SparkSession) = spark
     .read
     .option("header", "true")
     .csv("data/gdanskiewody/stations.csv")
-  println("Stations:")
-  println(s"#records: ${stations.count()}")
 
-  val schema = StructType(List(
-    StructField("Timestamp",TimestampType,false),
-    StructField("value",FloatType,true), 
-    StructField("station",IntegerType,false), 
-    StructField("channel",StringType,false)))
-  val sensors = spark
-    .read
-    .option("header", "true")
-    .option("timestampFormat", "y-M-d HH:mm:ss")
-    .schema(schema)
-    .csv("data/gdanskiewody/sensors")
+  def loadSensors(spark: SparkSession): DataFrame = {
+    val schema = StructType(List(
+      StructField("Timestamp",TimestampType,false),
+      StructField("value",FloatType,true), 
+      StructField("station",IntegerType,false), 
+      StructField("channel",StringType,false)))
+    spark
+      .read
+      .option("header", "true")
+      .option("timestampFormat", "y-M-d HH:mm:ss")
+      .schema(schema)
+      .csv("data/gdanskiewody/sensors")
+  }
 
-  sensors.head(10).foreach(println)
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .master("local[1]")
+      .appName("FindMissingReadings")
+      .getOrCreate()
 
-  spark.stop()
+    spark.sparkContext.setLogLevel("ERROR")
+    val stations = loadStations(spark)
+    val sensors = loadSensors(spark)
+  
+    println("Stations:")
+    println(s"#records: ${stations.count()}")
+    sensors.head(10).foreach(println)
+   
+    spark.stop()
+  }
+
 }
