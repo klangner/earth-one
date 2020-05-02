389096d406897e75035c698fef0262a502edc0b3
klangner <klangner@gmail.com>
2020-05-02 22:47:08 +0200

    Load configuration

--- crawler-rs/src/main.rs
+++ crawler-rs/src/main.rs
@@ -1,3 +1,30 @@
+use ini::Ini;
+
+
+#[derive(Debug)]
+struct Config {
+    api_key: String,
+    output_folder: String
+}
+
+impl Config {
+    fn load(file_path: &str) -> Config {
+        let conf = Ini::load_from_file(file_path).unwrap();
+        let section = conf.section(Some("Gdanskie Wody")).unwrap();
+        let api_key = section.get("apikey").unwrap();
+        let output_folder = section.get("output-folder").unwrap();
+        Config {api_key: api_key.to_owned(), output_folder: output_folder.to_owned()}
+    }
+}
+
+fn fetch_stations(config: &Config) -> Vec<String> {
+    vec![]
+}
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::load("production.config");
+    let stations = fetch_stations(&config);
+
+    println!("{:?}", config);
+    println!("{:?}", stations);
 }
