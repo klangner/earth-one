c7ac39d1f2ea8af7feb8ec0bf31646fa5e20aeaa
klangner <klangner@gmail.com>
2020-05-03 19:55:28 +0200

    List sensors

--- crawler-rs/src/main.rs
+++ crawler-rs/src/main.rs
@@ -5,6 +5,10 @@ use reqwest::header;
 use csv;
 
 
+/// List of channels
+static CHANNEL_NAMES: [&'static str; 8] = 
+    ["rain", "water", "winddir", "windlevel", "temp", "pressure", "humidity", "sun"];
+
 /// Program configuration read from the external file
 #[derive(Debug)]
 struct Config {
@@ -12,6 +16,7 @@ struct Config {
     output_folder: String
 }
 
+/// station data based on API
 #[derive(Deserialize, Serialize, Debug)]
 struct StationInfo {
     no: u32,
@@ -27,6 +32,7 @@ struct StationInfo {
     sun: bool
 }
 
+/// Response to the query about stations
 #[derive(Deserialize, Debug)]
 struct StationsResponse {
     status: String,
@@ -34,13 +40,39 @@ struct StationsResponse {
     data: Vec<StationInfo>
 }
 
+/// Single sensor description.
+#[derive(Debug)]
+struct Sensor {
+    station: u32,
+    channel: String
+}
+
+
 impl Config {
+    /// Configuration is stored in external file
     fn load(file_path: &str) -> Config {
         let conf = Ini::load_from_file(file_path).unwrap();
         let section = conf.section(Some("Gdanskie Wody")).unwrap();
         let api_key = section.get("apikey").unwrap();
         let output_folder = section.get("output-folder").unwrap();
         Config {api_key: api_key.to_owned(), output_folder: output_folder.to_owned()}
+    }
+}
+
+impl StationInfo {
+    /// Check if given channel is active
+    fn has_channel(&self, name: &str) -> bool {
+        match name {
+            "rain" => self.rain,
+            "water" => self.water,
+            "winddir" => self.winddir,
+            "windlevel" => self.windlevel,
+            "temp" => self.temp,
+            "pressure" => self.pressure,
+            "humidity" => self.humidity,
+            "sun" => self.sun,
+            _ => false
+        }
     }
 }
 
@@ -64,13 +96,27 @@ fn save_stations(stations: &Vec<StationInfo>, config: &Config) -> Result<(), csv
     Ok(())
 }
 
+
+/// Calculate list of activate sensors
+fn list_sensors(stations: &Vec<StationInfo>) -> Vec<Sensor> {
+    stations.iter()
+        .filter(|station| station.active)
+        .flat_map(|station| 
+            CHANNEL_NAMES.iter()
+                .filter(|name| station.has_channel(name))
+                .map(|name| Sensor{station: station.no, channel: name.to_string()} )
+                .collect::<Vec<Sensor>>())
+        .collect()
+}
+
+
 /// Main
 fn main() {
     let config = Config::load("production.config");
     let stations = fetch_stations(&config).unwrap();
     save_stations(&stations, &config).unwrap();
-    // List channels
+    let sensors = list_sensors(&stations);
     // Update channels
 
-    println!("{:?}", config);
+    sensors.iter().take(5).for_each(|c| println!("{:?}", c));
 }
