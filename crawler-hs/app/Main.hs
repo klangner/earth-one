e2bac419a7b3d568802df167bc91d8cecc9b1830
klangner <klangner@gmail.com>
2020-05-09 20:49:33 +0200

    New projects

--- crawler-hs/app/Main.hs
+++ crawler-hs/app/Main.hs
@@ -1,7 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
+
+import Data.Ini
+
+
+data Config = Config {apikey :: String, outputFolder :: String}
+    deriving Show
+
+
+loadConfig :: IO Config
+loadConfig = do
+    Right result <- readIniFile "production.config"
+    apikey <- (result >>= (lookupValue "Gdanskie Wody" "apikey"))
+    folder <- (result >>= (lookupValue "Gdanskie Wody" "output-folder"))
+    return (Config apikey folder)
 
 
 main :: IO ()
 main = do
-    config <- loadconfig
+    config <- loadConfig
+    putStrLn (show config)
+    return ()
 
